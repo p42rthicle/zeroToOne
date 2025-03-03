2c26b2fca9f82040960abea2e75ba15a5113738f
Parth Takkar <parthdroidworld@gmail.com>
2025-03-03 11:14:46 +0530

    basics

---
+++ basics/basicsJS.js
@@ -0,0 +1,10 @@
+console.log('hello, world');
+
+console.log('starry pattern');
+
+for (var i=0; i<4; i++) {
+    for (var j=0; j<i+1; j++) {
+        console.log("*")
+    }
+    console.log('\n')
+}
