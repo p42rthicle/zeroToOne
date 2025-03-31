c24968c8d632d06b3a47622a13c2b75ee50b9c69
Parth Takkar <parthdroidworld@gmail.com>
2025-03-31 12:48:38 +0530

    auth server js with Basic Auth

--- assignments/Week-2-assignments/authenticationServer.js
+++ assignments/Week-2-assignments/authenticationServer.js
@@ -29,9 +29,111 @@
   Testing the server - run `npm run test-authenticationServer` command in terminal
  */
 
-  const express = require("express")
-  const PORT = 3000;
-  const app = express();
-  // write your logic here, DONT WRITE app.listen(3000) when you're running tests, the tests will automatically start the server
+const express = require("express")
+const app = express();
+const port = 3000;
+var bodyParser = require('body-parser');
+
+let users = [];
+
+function generateId() {
+   return Math.random().toString(36).substring(2, 14) + Date.now().toString(36);
+}
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to authentication server');
+});
+
+// 1. POST /signup - User Signup
+app.post('/signup', (req, res) => {
+  const { username, password, firstName, lastName } = req.body;
+
+  if (!username || !password || !firstName) {
+    return res.status(400).send('Bad request: username, password and firstName are required');
+  } 
+  if (users.find(u => u.username === username)) {
+    return res.status(400).send('Bad request: username already exists');
+  }
+
+  const newUser = {
+    id: generateId(),
+    username: username,
+    password: password,
+    firstName: firstName,
+    lastName: lastName || ''
+  };
+  users.push(newUser);
+  res.status(201).send();
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).send('Bad Request: Missing credentials');
+  }
+  const user = users.find(u => u.username === username && u.password === password)
+  // If no user is found or password is incorrect
+  if (!user) {
+    return res.status(401).send('Unauthorized: Invalid credentials');
+  }
+
+  const token = Buffer.from(`${username}:${password}`).toString('base64');
+  const userResponse = {
+    token: token,
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName
+  }
+  res.status(200).json(userResponse);
+});
+
+// GET /data - Get user data
+app.get('/data', (req, res) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader || !authHeader.startsWith('Basic ')) {
+    return res.status(401).send('Unauthorized: Missing or Invalid Authorization header');
+  }
+
+  const base64Creds = authHeader.split(' ')[1];
+  let credentials;
+  try {
+    credentials = Buffer.from(base64Creds, 'base64').toString('utf-8');
+  } catch (e) {
+    return res.status(401).send('Unauthorized: Invalid Base64 encoding in Authorization header');
+  }
+
+  const [username, password] = credentials.split(':');
+  if (!username || !password) {
+    return res.status(401).send('Unauthorized: Invalid credentials format in header');
+  }
   
-  module.exports = app;
+  const authenticatedUser = users.find(u => u.username === username && u.password === password);
+
+  if (!authenticatedUser) {
+    return res.status(401).send('Unauthorized: Invalid username or password provided in the auth header')
+  }
+
+  //  --- Authorization Succeeded ---
+
+  const allUserData = users.map(u => ({
+    id: u.id,
+    firstName: u.firstName,
+    lastName: u.lastName
+  }));
+
+  res.status(200).json({users: allUserData});
+});
+
+app.use((req, res) => {
+  res.status(404).send('Route not found');
+});
+
+app.listen(port, () => {
+  console.log(`Server started running on port ${port}`);
+});
+
+module.exports = app;
